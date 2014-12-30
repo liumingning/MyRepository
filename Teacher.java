00734229103c9a9af2099a6ae3513d509099f4d4
liumingning <liumingning@126.com>
2014-12-30 16:38:07 +0800

    创建Teacher类
    创建Teacher类

---
+++ Teacher.java
@@ -0,0 +1,17 @@
+public class teacher
+{
+   public String name;
+   public String gender;
+   public int    age;
+   public void setName(String name)
+ { 
+     this.name=name; 
+  }
+  public String getName()
+  { 
+     return this.name;
+   
+  } 
+
+
+}
